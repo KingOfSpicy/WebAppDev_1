ba71655492ad68e0fc376d3e10463e1a1559a441
KingOfSpicy <sangowosan@gmail.com>
2022-03-10 09:15:49 +0200

    add class necessary elements.

---
+++ Data/CheWei_WAD_Task1_2019verContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheWei_WAD_Task1_2019ver.Models
+{
+    public class CheWei_WAD_Task1_2019verContext : DbContext
+    {
+        public CheWei_WAD_Task1_2019verContext (DbContextOptions<CheWei_WAD_Task1_2019verContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CheWei_WAD_Task1_2019ver.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CheWei_WAD_Task1_2019ver.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
